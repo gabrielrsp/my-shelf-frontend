cdfa8a218530d9883dd56733e45f286812435713
gabrielrsp <gabrielrsp18@gmail.com>
2020-05-16 23:09:11 -0300

    add profile validations

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,27 +7,41 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
 
-  try {
+  const { name, email, ...rest } = payload.data;
 
-    const { name, email, ...rest } = payload.data;
+  const profile = Object.assign(
+    { name, email },
+    rest.oldPassword ? rest : {}
+  );
 
-    const profile = Object.assign(
-      { name, email },
-      rest.oldPassword ? rest : {}
-    );
+  console.log(profile)
 
-    const response = yield call(api.put, 'users', profile);
+  if (rest.password) {
 
-    toast.success('Profile updated successfully');
-    yield put(updateProfileSuccess(response.data));
+    if (rest.password !== rest.confirmPassword) {
+      toast.error('confirmation password is not matching with new password');
+      yield put(updateProfileFailure())
+    }
 
-  } catch (err) {
-
-    toast.error('Failed to update Profile');
-    yield put(updateProfileFailure())
+    if ((rest.password.length < 6) || (rest.confirmPassword.length < 6)) {
+      toast.error('password must have 6 characters at least');
+      yield put(updateProfileFailure())
+      return
+    }
 
   }
-}
+
+    try {
+      const response = yield call(api.put, 'users', profile);
+      toast.success('Profile updated');
+      yield put(updateProfileSuccess(response.data));
+    }
+    catch {
+      toast.error('failed to update');
+      yield put(updateProfileFailure());
+    }
+  }
+
 
 export default all([
   takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)
