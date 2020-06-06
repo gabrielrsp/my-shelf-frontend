4943fd16bd06c964b7537e529ee3f289ca6072cf
gabrielrsp <gabrielrsp18@gmail.com>
2020-06-06 19:51:48 -0300

    redirect to login page when token is invalid

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { BookList, Container, Form, SubmitButton, UpdateButton, MainBody } from './styles';
 import { FaPlus, FaEdit, FaTimes } from "react-icons/fa";
 import BookItem from '../../components/BookItem';
+import history from '../../../services/history';
 import api from '../../services/api';
 import { motion } from 'framer-motion';
 
@@ -16,18 +17,15 @@ export default function Main() {
   const [box, setBox] = useState();
   const [idClick, setIdClick] = useState(1);
 
-
   useEffect(() => {
     async function loadBooks() {
+      const response = await api.get('books')
+        .catch(function (error) {
+          if (error.response.status == 401) {
+            history.push('/');
+          }
+        });
 
-      const response = await api.get('books').catch(function (error) {
-        if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-      
       setBook(response.data)
     }
 
