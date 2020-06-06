b9f3c692123c578e524e329ae8e8b1ea8fe3c937
gabrielrsp <gabrielrsp18@gmail.com>
2020-06-06 19:38:40 -0300

    refactor

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -20,7 +20,9 @@ export default function Main() {
   useEffect(() => {
     async function loadBooks() {
       const response = await api.get('books');
-      console.log(response)
+      if(!response){
+        console.log('response nao preenchido')
+      } else
       setBook(response.data)
     }
 
